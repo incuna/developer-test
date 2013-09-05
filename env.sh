43fb42e25a49f162c15162680b486988ffebbe92
Max Peterson <max@incuna.com>
2013-09-05 17:14:25 +0100

    Fix default DATABASE_URL.

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export DEBUG=True
-export DATABASE_URL=`pwd`/db.sqlite
+export DATABASE_URL=sqlite:///`pwd`/db.sqlite
 export CSS_DEBUG=False
 export EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
